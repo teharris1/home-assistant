f7c2e46b59fd3be9e4af0b8dcf454fa7033b11ba
Tom Harris <tomharris@harrisnj.net>
2021-02-03 15:19:58 +0000

    Add APIs for UI

--- homeassistant/components/insteon/schemas.py
+++ homeassistant/components/insteon/schemas.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from binascii import Error as HexError, unhexlify
 import re
-from typing import Union
+from typing import Dict, Union
 
 import voluptuous as vol
 
@@ -169,6 +169,20 @@ TRIGGER_SCENE_SCHEMA = vol.Schema(
 
 
 ADD_DEFAULT_LINKS_SCHEMA = vol.Schema({vol.Required(CONF_ENTITY_ID): cv.entity_id})
+
+
+ALDB_SCHEMA = vol.Schema(
+    {
+        vol.Required("in_use"): bool,
+        vol.Required("mode"): vol.In(["Controller", "Responder"]),
+        vol.Required("group"): int,
+        vol.Required("target"): str,
+        vol.Required("data1"): int,
+        vol.Required("data2"): int,
+        vol.Required("data3"): int,
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
 
 def normalize_byte_entry_to_int(entry: Union[int, bytes, str]):
