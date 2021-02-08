3e514805fc6e54c27c751d63d634c1e31a83afae
Tom Harris <tomharris@harrisnj.net>
2021-02-08 01:12:11 +0000

    ALDB record using byte not int

--- homeassistant/components/insteon/schemas.py
+++ homeassistant/components/insteon/schemas.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 
 from binascii import Error as HexError, unhexlify
 
-from pyinsteon.address import Address
-from pyinsteon.constants import HC_LOOKUP
 import voluptuous as vol
 
 from homeassistant.const import (
@@ -19,6 +17,8 @@ from homeassistant.const import (
     ENTITY_MATCH_ALL,
 )
 import homeassistant.helpers.config_validation as cv
+from pyinsteon.address import Address
+from pyinsteon.constants import HC_LOOKUP
 
 from .const import (
     CONF_CAT,
@@ -169,17 +169,20 @@ TRIGGER_SCENE_SCHEMA = vol.Schema(
 ADD_DEFAULT_LINKS_SCHEMA = vol.Schema({vol.Required(CONF_ENTITY_ID): cv.entity_id})
 
 
-ALDB_SCHEMA = vol.Schema(
+ALDB_RECORD_SCHEMA = vol.Schema(
     {
+        vol.Required("mem_addr"): int,
         vol.Required("in_use"): bool,
-        vol.Required("mode"): vol.In(["Controller", "Responder"]),
-        vol.Required("group"): int,
+        vol.Required("group"): vol.Range(0, 255),
+        vol.Required("mode"): vol.In(["c", "C", "R", "r"]),
+        vol.Optional("highwater"): bool,
         vol.Required("target"): str,
-        vol.Required("data1"): int,
-        vol.Required("data2"): int,
-        vol.Required("data3"): int,
-    },
-    extra=vol.ALLOW_EXTRA,
+        vol.Optional("target_name"): str,
+        vol.Required("data1"): vol.Range(0, 255),
+        vol.Required("data2"): vol.Range(0, 255),
+        vol.Required("data3"): vol.Range(0, 255),
+        vol.Optional("dirty"): bool,
+    }
 )
 
 
