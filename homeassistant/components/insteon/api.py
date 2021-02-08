c8292ed21505fca14e51d1756e47f9600a8d74ef
Tom Harris <tomharris@harrisnj.net>
2021-02-08 01:11:24 +0000

    Clean up api

--- homeassistant/components/insteon/api.py
+++ homeassistant/components/insteon/api.py
@@ -1,34 +1,31 @@
 """Web socket API for Insteon devices."""
-import logging
 
-from pyinsteon import devices
 import voluptuous as vol
-import voluptuous_serialize
 
 from homeassistant.components import websocket_api
 from homeassistant.core import callback
+from pyinsteon import devices
+from pyinsteon.constants import ALDBStatus
+from pyinsteon.topics import (
+    ALDB_STATUS_CHANGED,
+    DEVICE_LINK_CONTROLLER_CREATED,
+    DEVICE_LINK_RESPONDER_CREATED,
+)
+from pyinsteon.utils import subscribe_topic, unsubscribe_topic
 
 from .const import DOMAIN
-from .schemas import ALDB_SCHEMA
+from .properties import get_properties, set_property
+from .schemas import ALDB_RECORD_SCHEMA
 
-_LOGGER = logging.getLogger(__name__)
 TYPE = "type"
 ID = "id"
 DEVICE_ID = "device_id"
+DEVICE_ADDRESS = "device_address"
 ALDB_RECORD = "record"
-
-
-async def async_get_devices(hass, connection, msg):
-    """Get the HA and Insteon devices."""
-    ha_device = await async_get_ha_device(hass, msg[DEVICE_ID])
-    if not ha_device:
-        notify_device_not_found(connection, msg, "HA device not found.")
-        return None, None
-    device = get_insteon_device(ha_device)
-    if not device:
-        notify_device_not_found(connection, msg, "Insteon device not found.")
-        return ha_device, None
-    return ha_device, device
+PROPERTY_NAME = "name"
+PROPERTY_VALUE = "value"
+HA_DEVICE_NOT_FOUND = "ha_device_not_found"
+INSTEON_DEVICE_NOT_FOUND = "insteon_device_not_found"
 
 
 def compute_device_name(ha_device):
@@ -36,20 +33,15 @@ def compute_device_name(ha_device):
     return ha_device.name_by_user if ha_device.name_by_user else ha_device.name
 
 
-async def async_get_ha_device(hass, device_id):
-    """Get the Insteon device from a device registry id."""
-    dev_registry = await hass.helpers.device_registry.async_get_registry()
-    return dev_registry.async_get(device_id)
-
-
-def get_insteon_device(ha_device):
+def get_insteon_device_from_ha_device(ha_device):
     """Return the Insteon device from an HA device."""
     for identifier in ha_device.identifiers:
-        address = identifier[1]
-    return devices[address]
+        if len(identifier) > 1 and identifier[0] == DOMAIN and devices[identifier[1]]:
+            return devices[identifier[1]]
+    return None
 
 
-async def async_get_device_name_from_address(dev_registry, address):
+async def async_device_name(dev_registry, address):
     """Get the Insteon device name from a device registry id."""
     ha_device = dev_registry.async_get_device(
         identifiers={(DOMAIN, str(address))}, connections=set()
@@ -62,9 +54,8 @@ async def async_get_device_name_from_address(dev_registry, address):
     return compute_device_name(ha_device)
 
 
-async def async_record_to_dict(hass, record):
+async def async_aldb_record_to_dict(dev_registry, record, dirty=False):
     """Convert an ALDB record to a dict."""
-    dev_registry = await hass.helpers.device_registry.async_get_registry()
     return {
         "mem_addr": record.mem_addr,
         "in_use": record.is_in_use,
@@ -72,22 +63,12 @@ async def async_record_to_dict(hass, record):
         "highwater": record.is_high_water_mark,
         "group": record.group,
         "target": str(record.target),
-        "target_name": await async_get_device_name_from_address(
-            dev_registry, record.target
-        ),
+        "target_name": await async_device_name(dev_registry, record.target),
         "data1": record.data1,
         "data2": record.data2,
         "data3": record.data3,
-        "dirty": False,
+        "dirty": dirty,
     }
-
-
-async def async_get_aldb_records(hass, aldb):
-    """Get all ALDB records and changes."""
-    records = {
-        mem_addr: await async_record_to_dict(hass, aldb[mem_addr]) for mem_addr in aldb
-    }
-    return await async_merge_pending_changes(hass, records, aldb.get_pending())
 
 
 def notify_device_not_found(connection, msg, text):
@@ -95,40 +76,15 @@ def notify_device_not_found(connection, msg, text):
     connection.send_message(
         websocket_api.error_message(msg[ID], websocket_api.const.ERR_NOT_FOUND, text)
     )
-    return
-
-
-def default_link_to_dict(link):
-    """Convert a default link to a dict."""
-    return {
-        "is_controller": link.is_controller,
-        "group": link.group,
-        "dev_data1": link.dev_data1,
-        "dev_data2": link.dev_data2,
-        "dev_data3": link.dev_data3,
-        "modem_data1": link.modem_data1,
-        "modem_data2": link.modem_data2,
-        "modem_data3": link.modem_data3,
-    }
 
 
 async def async_reload_and_save_aldb(hass, device):
     """Add default links to an Insteon device."""
-    await device.aldb.async_load(refresh=True)
+    if device == devices.modem:
+        await device.aldb.async_load()
+    else:
+        await device.aldb.async_load(refresh=True)
     await devices.async_save(workdir=hass.config.config_dir)
-
-
-async def async_merge_pending_changes(hass, aldb, changes):
-    """Merge the ALDB records with pending changes."""
-    next_new_id = -1
-    for rec in changes:
-        if rec.mem_addr == 0:
-            rec.mem_addr = next_new_id
-            next_new_id -= 1
-        aldb[rec.mem_addr] = await async_record_to_dict(hass, rec)
-        aldb[rec.mem_addr]["dirty"] = True
-        _LOGGER.info("Record %d is dirty", rec.mem_addr)
-    return aldb
 
 
 @websocket_api.require_admin
@@ -138,8 +94,14 @@ async def async_merge_pending_changes(hass, aldb, changes):
 )
 async def websocket_get_device(hass, connection, msg):
     """Get an Insteon device."""
-    ha_device, device = await async_get_devices(hass, connection, msg)
-    if not ha_device or not device:
+    dev_registry = await hass.helpers.device_registry.async_get_registry()
+    ha_device = dev_registry.async_get(msg[DEVICE_ID])
+    if not ha_device:
+        notify_device_not_found(connection, msg, HA_DEVICE_NOT_FOUND)
+        return
+    device = get_insteon_device_from_ha_device(ha_device)
+    if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
     ha_name = compute_device_name(ha_device)
     device_info = {
@@ -147,7 +109,6 @@ async def websocket_get_device(hass, connection, msg):
         "address": str(device.address),
         "is_battery": device.is_battery,
         "aldb_status": str(device.aldb.status),
-        "default_links": [default_link_to_dict(link) for link in device.default_links],
     }
     connection.send_result(msg[ID], device_info)
 
@@ -155,26 +116,30 @@ async def websocket_get_device(hass, connection, msg):
 @websocket_api.require_admin
 @websocket_api.async_response
 @websocket_api.websocket_command(
-    {vol.Required(TYPE): "insteon/aldb/get", vol.Required(DEVICE_ID): str}
+    {vol.Required(TYPE): "insteon/aldb/get", vol.Required(DEVICE_ADDRESS): str}
 )
 async def websocket_get_aldb(hass, connection, msg):
     """Get the All-Link Database for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
 
-    records = await async_get_aldb_records(hass, device.aldb)
+    # Convert the ALDB to a dict merge in pending changes
+    aldb = {mem_addr: device.aldb[mem_addr] for mem_addr in device.aldb}
+    aldb.update(device.aldb.pending_changes)
+    changed_records = list(device.aldb.pending_changes.keys())
 
-    aldb_info = {
-        "schema": voluptuous_serialize.convert(
-            ALDB_SCHEMA
-        ),  # , custom_serializer=cv.voluptuous_serialize),
-        "records": [v for v in records.values()],
-    }
-    for rec in aldb_info["records"]:
-        if rec["dirty"]:
-            _LOGGER.error("Record %d is being sent dirty", rec["mem_addr"])
-    connection.send_result(msg[ID], aldb_info)
+    dev_registry = await hass.helpers.device_registry.async_get_registry()
+
+    records = [
+        await async_aldb_record_to_dict(
+            dev_registry, aldb[mem_addr], mem_addr in changed_records
+        )
+        for mem_addr in aldb
+    ]
+
+    connection.send_result(msg[ID], records)
 
 
 @websocket_api.require_admin
@@ -182,15 +147,15 @@ async def websocket_get_aldb(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/aldb/change",
-        vol.Required(DEVICE_ID): str,
-        vol.Required(ALDB_RECORD): dict,
+        vol.Required(DEVICE_ADDRESS): str,
+        vol.Required(ALDB_RECORD): ALDB_RECORD_SCHEMA,
     }
 )
 async def websocket_change_aldb_record(hass, connection, msg):
     """Change an All-Link Database record for an Insteon device."""
-    _LOGGER.info("Change started")
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
 
     record = msg[ALDB_RECORD]
@@ -204,6 +169,7 @@ async def websocket_change_aldb_record(hass, connection, msg):
         data2=record["data2"],
         data3=record["data3"],
     )
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -211,14 +177,15 @@ async def websocket_change_aldb_record(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/aldb/create",
-        vol.Required(DEVICE_ID): str,
-        vol.Required(ALDB_RECORD): dict,
+        vol.Required(DEVICE_ADDRESS): str,
+        vol.Required(ALDB_RECORD): ALDB_RECORD_SCHEMA,
     }
 )
 async def websocket_create_aldb_record(hass, connection, msg):
     """Create an All-Link Database record for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
 
     record = msg[ALDB_RECORD]
@@ -230,6 +197,7 @@ async def websocket_create_aldb_record(hass, connection, msg):
         data2=record["data2"],
         data3=record["data3"],
     )
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -237,17 +205,19 @@ async def websocket_create_aldb_record(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/aldb/write",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
     }
 )
 async def websocket_write_aldb(hass, connection, msg):
     """Create an All-Link Database record for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
 
     await device.aldb.async_write()
     hass.async_create_task(async_reload_and_save_aldb(hass, device))
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -255,16 +225,18 @@ async def websocket_write_aldb(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/aldb/load",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
     }
 )
 async def websocket_load_aldb(hass, connection, msg):
     """Create an All-Link Database record for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
 
     hass.async_create_task(async_reload_and_save_aldb(hass, device))
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -272,16 +244,18 @@ async def websocket_load_aldb(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/aldb/reset",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
     }
 )
 async def websocket_reset_aldb(hass, connection, msg):
     """Create an All-Link Database record for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
 
     device.aldb.clear_pending()
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -289,18 +263,20 @@ async def websocket_reset_aldb(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/aldb/add_default_links",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
     }
 )
 async def websocket_add_default_links(hass, connection, msg):
     """Add the default All-Link Database records for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
 
     device.aldb.clear_pending()
     await device.async_add_default_links()
     hass.async_create_task(async_reload_and_save_aldb(hass, device))
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -308,14 +284,19 @@ async def websocket_add_default_links(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/properties/get",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
     }
 )
 async def websocket_get_properties(hass, connection, msg):
     """Add the default All-Link Database records for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
+
+    properties, schema = get_properties(device)
+
+    connection.send_result(msg[ID], {"properties": properties, "schema": schema})
 
 
 @websocket_api.require_admin
@@ -323,14 +304,20 @@ async def websocket_get_properties(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/properties/change",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
+        vol.Required(PROPERTY_NAME): str,
+        vol.Required(PROPERTY_VALUE): vol.Any(list, int, float, bool, str),
     }
 )
 async def websocket_change_properties_record(hass, connection, msg):
     """Add the default All-Link Database records for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
+
+    set_property(device, msg[PROPERTY_NAME], msg[PROPERTY_VALUE])
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -338,14 +325,19 @@ async def websocket_change_properties_record(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/properties/write",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
     }
 )
 async def websocket_write_properties(hass, connection, msg):
     """Add the default All-Link Database records for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
+
+    await device.async_write_op_flags()
+    await device.async_write_ext_properties()
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -353,14 +345,19 @@ async def websocket_write_properties(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/properties/load",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
     }
 )
 async def websocket_load_properties(hass, connection, msg):
     """Add the default All-Link Database records for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
+
+    await device.async_read_op_flags()
+    await device.async_read_ext_properties()
+    connection.send_result(msg[ID])
 
 
 @websocket_api.require_admin
@@ -368,14 +365,70 @@ async def websocket_load_properties(hass, connection, msg):
 @websocket_api.websocket_command(
     {
         vol.Required(TYPE): "insteon/properties/reset",
-        vol.Required(DEVICE_ID): str,
+        vol.Required(DEVICE_ADDRESS): str,
     }
 )
 async def websocket_reset_properties(hass, connection, msg):
     """Add the default All-Link Database records for an Insteon device."""
-    _, device = await async_get_devices(hass, connection, msg)
+    device = devices[msg[DEVICE_ADDRESS]]
     if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
         return
+
+    for prop in device.operating_flags:
+        device.operating_flags[prop].new_value = None
+    for prop in device.properties:
+        device.properties[prop].new_value = None
+    connection.send_result(msg[ID])
+
+
+@websocket_api.require_admin
+@websocket_api.async_response
+@websocket_api.websocket_command(
+    {
+        vol.Required(TYPE): "insteon/aldb/notify",
+        vol.Required(DEVICE_ADDRESS): str,
+    }
+)
+async def websocket_notify_on_aldb_status(hass, connection, msg):
+    """Tell Insteon a new ALDB record was added."""
+
+    device = devices[msg[DEVICE_ADDRESS]]
+    if not device:
+        notify_device_not_found(connection, msg, INSTEON_DEVICE_NOT_FOUND)
+        return
+
+    @callback
+    def record_added(controller, responder, group):
+        """Forward ALDB events to websocket."""
+        forward_data = {"type": "record_loaded"}
+        connection.send_message(websocket_api.event_message(msg["id"], forward_data))
+
+    @callback
+    def aldb_loaded():
+        """Forward ALDB loaded event to websocket."""
+        forward_data = {
+            "type": "status_changed",
+            "is_loading": device.aldb.status == ALDBStatus.LOADING,
+        }
+        connection.send_message(websocket_api.event_message(msg["id"], forward_data))
+
+    @callback
+    def async_cleanup() -> None:
+        """Remove signal listeners."""
+        unsubscribe_topic(record_added, f"{DEVICE_LINK_CONTROLLER_CREATED}.{device.id}")
+        unsubscribe_topic(record_added, f"{DEVICE_LINK_RESPONDER_CREATED}.{device.id}")
+        unsubscribe_topic(aldb_loaded, f"{device.id}.{ALDB_STATUS_CHANGED}")
+
+        forward_data = {"type": "unsubscribed"}
+        connection.send_message(websocket_api.event_message(msg["id"], forward_data))
+
+    connection.subscriptions[msg["id"]] = async_cleanup
+    subscribe_topic(record_added, f"{DEVICE_LINK_CONTROLLER_CREATED}.{device.id}")
+    subscribe_topic(record_added, f"{DEVICE_LINK_RESPONDER_CREATED}.{device.id}")
+    subscribe_topic(aldb_loaded, f"{device.id}.{ALDB_STATUS_CHANGED}")
+
+    connection.send_result(msg[ID])
 
 
 @callback
@@ -389,6 +442,7 @@ def async_load_api(hass):
     websocket_api.async_register_command(hass, websocket_load_aldb)
     websocket_api.async_register_command(hass, websocket_reset_aldb)
     websocket_api.async_register_command(hass, websocket_add_default_links)
+    websocket_api.async_register_command(hass, websocket_notify_on_aldb_status)
 
     websocket_api.async_register_command(hass, websocket_get_properties)
     websocket_api.async_register_command(hass, websocket_change_properties_record)
